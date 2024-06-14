e7964c0387278a0d25cc931af362ee2dedb68d24
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-14 14:10:27 -0500

    add Position attributes

---
+++ src/main/java/com/angelou/futplayerapi/model/entity/Position.java
@@ -0,0 +1,21 @@
+package com.angelou.futplayerapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "position")
+public class Position {
+    @Id
+    @Column(name = "position_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
