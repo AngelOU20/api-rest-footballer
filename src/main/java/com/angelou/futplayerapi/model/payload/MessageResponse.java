80f1479a1f4486f83e55fab49c980700ba5692af
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-18 15:04:14 -0500

    add MessageResponse

---
+++ src/main/java/com/angelou/futplayerapi/model/payload/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.angelou.futplayerapi.model.payload;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@ToString
+public class MessageResponse implements Serializable {
+    private String message;
+    private Object object;
+}
