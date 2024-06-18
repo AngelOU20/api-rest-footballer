92ec59b620e784c60d25d1e30863e9adeaaae31c
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-17 19:30:59 -0500

    initial commit

---
+++ src/main/java/com/angelou/futplayerapi/FutplayerApiApplication.java
@@ -0,0 +1,13 @@
+package com.angelou.futplayerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutplayerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutplayerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angelou/futplayerapi/FutplayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelou.futplayerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutplayerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
